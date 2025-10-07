9435abd502d8fc80dfe68e19469bd06c2da6841e
Dan Fink <Daniel.Fink@cognizant.com>
2025-10-06 17:51:50 -0700

    fix flake8

--- neuro_san/internals/run_context/langchain/llms/azure_llm_policy.py
+++ neuro_san/internals/run_context/langchain/llms/azure_llm_policy.py
@@ -69,7 +69,7 @@ class AzureLlmPolicy(OpenAILlmPolicy):
             azure_endpoint=self.get_value_or_env(config, "azure_endpoint",
                                                  "AZURE_OPENAI_ENDPOINT"),
             azure_deployment=self.get_value_or_env(config, "deployment_name",
-                                                  "AZURE_OPENAI_DEPLOYMENT_NAME"),
+                                                   "AZURE_OPENAI_DEPLOYMENT_NAME"),
             api_version=self.get_value_or_env(config, "openai_api_version",
                                               "OPENAI_API_VERSION"),
             api_key=openai_api_key,
