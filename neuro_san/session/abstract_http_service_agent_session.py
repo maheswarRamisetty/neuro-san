22b6c27b38a5ba41d2aa0ae912c0782c6abbf985
Dan Fink <Daniel.Fink@cognizant.com>
2026-02-10 21:57:12 -0800

    refine message

--- neuro_san/session/abstract_http_service_agent_session.py
+++ neuro_san/session/abstract_http_service_agent_session.py
@@ -129,6 +129,6 @@ class AbstractHttpServiceAgentSession(AgentSessionConstants):
            need to be well known.  If you're unfamiliar with this process, it's a big deal.
            Try regular http instead.
         10. Is your server running running with an AGENT_AUTHORIZER?  If so check your
-            authorization policy/database to be sure the user has the correct permissions.
+            authorization policy/database to be sure the user has the correct permissions for the network.
         """
         return message
