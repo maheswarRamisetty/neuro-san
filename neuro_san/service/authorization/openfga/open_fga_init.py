bd0da0adb8ff3d76beec2c57bb962a9b4bbaf692
Dan Fink <Daniel.Fink@cognizant.com>
2026-02-12 11:23:24 -0800

    Use async with

--- neuro_san/service/authorization/openfga/open_fga_init.py
+++ neuro_san/service/authorization/openfga/open_fga_init.py
@@ -177,7 +177,7 @@ class OpenFgaInit:
 
         # Read the OpenFGA policy from configuration
         policy: Dict[str, Any] = {}
-        with aiofiles.open(open_fga_policy_file, "r", encoding="utf-8") as policy_file:
+        async with aiofiles.open(open_fga_policy_file, "r", encoding="utf-8") as policy_file:
             policy = json.load(policy_file)
 
         found_auth_model: str = await self.find_auth_model(open_fga_client, policy)
